57aed8af3f9b53c7b3bc41785511524a45a06b78
NintySwinty <emil.panecki@gmail.com>
2024-05-05 21:43:41 +0200

    Formatting

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -22,42 +22,42 @@ MainWindow::MainWindow(QWidget *parent)
                    [&] { ui->stackedWidget->setCurrentIndex(0); });
 
   QObject::connect(chatSocket, &QTcpSocket::readyRead, [&] {
-      QByteArray messageByte = chatSocket->readAll();
-      chatSocket->flush();
-      QString message = messageByte;
-      qDebug() << "Message: " + message;
+    QByteArray messageByte = chatSocket->readAll();
+    chatSocket->flush();
+    QString message = messageByte;
+    qDebug() << "Message: " + message;
 
-      QJsonParseError JsonError;
-      QJsonDocument Document = QJsonDocument::fromJson(messageByte, &JsonError);
-      if (JsonError.error != QJsonParseError::NoError) {
-        qDebug() << "Error In Json Data : " << JsonError.errorString();
-        return;
+    QJsonParseError JsonError;
+    QJsonDocument Document = QJsonDocument::fromJson(messageByte, &JsonError);
+    if (JsonError.error != QJsonParseError::NoError) {
+      qDebug() << "Error In Json Data : " << JsonError.errorString();
+      return;
+    }
+
+    if (Document.isObject()) {
+      QJsonObject Obj = Document.object();
+
+      QJsonArray users = Obj.value("Users").toArray();
+      QString message = Obj.value("Message").toString();
+
+      qDebug() << Obj;
+
+      if (!users.isEmpty()) {
+        qDebug() << "Przydzielanie aktywnych userów!";
+        ui->listWidget_3->clear();
+
+        for (auto user : users) {
+          QJsonObject ObjUsers = user.toObject();
+          ui->listWidget_3->addItem(ObjUsers.value("Nick").toString());
+        }
       }
 
-      if (Document.isObject()) {
-        QJsonObject Obj = Document.object();
-
-        QJsonArray users = Obj.value("Users").toArray();
-        QString message = Obj.value("Message").toString();
-
-        qDebug() << Obj;
-
-        if (!users.isEmpty()) {
-          qDebug() << "Przydzielanie aktywnych userów!";
-          ui->listWidget_3->clear();
-
-          for (auto user : users) {
-            QJsonObject ObjUsers = user.toObject();
-            ui->listWidget_3->addItem(ObjUsers.value("Nick").toString());
-          }
-        }
-
-        if (message != "") {
-            qDebug() << "Doszła wiadomość!";
-            ui->listWidget_2->addItem(Obj.value("Nick").toString() + ": " +
-                                    Obj.value("Message").toString() + " - " +
-                                    Obj.value("Date").toString());
-        }
+      if (message != "") {
+        qDebug() << "Doszła wiadomość!";
+        ui->listWidget_2->addItem(Obj.value("Nick").toString() + ": " +
+                                  Obj.value("Message").toString() + " - " +
+                                  Obj.value("Date").toString());
+      }
     }
   });
 
@@ -82,13 +82,13 @@ MainWindow::MainWindow(QWidget *parent)
 
   //         int frame_size = 960;
   //         opus_int16 decoded_data[48000*2];
-  //         int decoded_samples = opus_decode(decoder, (const unsigned char*)datagram.data(), datagram.size(), decoded_data, frame_size, 0);
-  //         if (decoded_samples > 0) {
-  //             QByteArray data((const char*)decoded_data, decoded_samples * sizeof(opus_int16));
-  //             audioOutput->start()->write(data);
+  //         int decoded_samples = opus_decode(decoder, (const unsigned
+  //         char*)datagram.data(), datagram.size(), decoded_data, frame_size,
+  //         0); if (decoded_samples > 0) {
+  //             QByteArray data((const char*)decoded_data, decoded_samples *
+  //             sizeof(opus_int16)); audioOutput->start()->write(data);
   //     }
   // });
-
 
   // QAudioSource *audioSource = new QAudioSource();
   // QIODevice *inputDevice = audioSource->start();
@@ -97,15 +97,21 @@ MainWindow::MainWindow(QWidget *parent)
   //       // Encode data...
   //       int frame_size = 960;
   //       unsigned char encoded_data[4000];
-  //       int encoded_bytes = opus_encode(encoder, (const opus_int16*)data.data(), frame_size, encoded_data, 4000);
-  //       if (encoded_bytes > 0) {
-  //         voiceSocket->writeDatagram((const char*)encoded_data, encoded_bytes, QHostAddress::LocalHost, 8081);
+  //       int encoded_bytes = opus_encode(encoder, (const
+  //       opus_int16*)data.data(), frame_size, encoded_data, 4000); if
+  //       (encoded_bytes > 0) {
+  //         voiceSocket->writeDatagram((const char*)encoded_data,
+  //         encoded_bytes, QHostAddress::LocalHost, 8081);
   //       }
   //   });
-
 }
 
-MainWindow::~MainWindow() { /* opus_encoder_destroy(encoder); opus_decoder_destroy(decoder); */ delete chatSocket; /* delete voiceSocket; delete audioInput; delete audioOutput; delete format; */ delete ui; }
+MainWindow::~MainWindow() { /* opus_encoder_destroy(encoder);
+                               opus_decoder_destroy(decoder); */
+  delete chatSocket;        /* delete voiceSocket; delete audioInput; delete
+                               audioOutput; delete format; */
+  delete ui;
+}
 
 void MainWindow::on_pushButton_5_clicked() {
   ui->plainTextEdit_2->toPlainText();  // IP
@@ -138,31 +144,31 @@ void MainWindow::on_pushButton_6_clicked() {
   ui->textEdit->setText("");
 }
 
-void MainWindow::on_pushButton_3_clicked()
-{
-    if(ui->pushButton_3->text() == "Mute") {
-        ui->pushButton_3->setText("Unmute");
-        // audioInput->setMuted(true);
-    }
+void MainWindow::on_pushButton_3_clicked() {
+  if (ui->pushButton_3->text() == "Mute") {
+    ui->pushButton_3->setText("Unmute");
+    // audioInput->setMuted(true);
+  }
 
-    if(ui->pushButton_3->text() == "Unmute") {
-        ui->pushButton_3->setText("Mute");
-        // audioInput->setMuted(false);
-    }
+  if (ui->pushButton_3->text() == "Unmute") {
+    ui->pushButton_3->setText("Mute");
+    // audioInput->setMuted(false);
+  }
 }
 
-void MainWindow::keyPressEvent(QKeyEvent *event)
-{
-    if( ( ( event->key() == Qt::Key_Return ) && ui->stackedWidget->currentIndex() == 1 ) && event->key() != Qt::Key_Shift ) {
-        chatSocket->write(
-            "{"
-            " \"Message\": \"" +
-            ui->textEdit->toPlainText().toUtf8() +
-            "\""
-            "}");
-        chatSocket->flush();
-        ui->textEdit->setText("");
-    }
+void MainWindow::keyPressEvent(QKeyEvent *event) {
+  if (((event->key() == Qt::Key_Return) &&
+       ui->stackedWidget->currentIndex() == 1) &&
+      event->key() != Qt::Key_Shift) {
+    chatSocket->write(
+        "{"
+        " \"Message\": \"" +
+        ui->textEdit->toPlainText().toUtf8() +
+        "\""
+        "}");
+    chatSocket->flush();
+    ui->textEdit->setText("");
+  }
 }
 
 // bool MainWindow::eventFilter(QObject *obj, QEvent *event)
